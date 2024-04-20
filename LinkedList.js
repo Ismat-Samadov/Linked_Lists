62c07b6e1038502236203ae5f94983f71ee2bc5b
Ismat-Samadov <ismetsemedov@gmail.com>
2024-04-20 17:21:56 +0400

    update for LinkedList.js

--- LinkedList.js
+++ LinkedList.js
@@ -1,5 +1,6 @@
 class Node {
     constructor(value) {
+        // INITIALIZE NODE WITH VALUE AND NEXTNODE SET TO NULL
         this.value = value;
         this.nextNode = null;
     }
@@ -7,12 +8,14 @@ class Node {
 
 class LinkedList {
     constructor() {
+        // INITIALIZE LINKED LIST WITH HEAD, TAIL, AND LENGTH SET TO NULL AND 0 RESPECTIVELY
         this.head = null;
         this.tail = null;
         this.length = 0;
     }
 
     append(value) {
+        // ADD A NEW NODE CONTAINING VALUE TO THE END OF THE LIST
         const newNode = new Node(value);
         if (!this.head) {
             this.head = newNode;
@@ -25,6 +28,7 @@ class LinkedList {
     }
 
     prepend(value) {
+        // ADD A NEW NODE CONTAINING VALUE TO THE START OF THE LIST
         const newNode = new Node(value);
         if (!this.head) {
             this.head = newNode;
@@ -37,20 +41,23 @@ class LinkedList {
     }
 
     size() {
+        // RETURN THE TOTAL NUMBER OF NODES IN THE LIST
         return this.length;
     }
 
     head() {
+        // RETURN THE HEAD NODE OF THE LIST
         return this.head;
     }
 
     tail() {
+        // RETURN THE TAIL NODE OF THE LIST
         return this.tail;
     }
 
     at(index) {
+        // RETURN THE NODE AT THE GIVEN INDEX
         if (index < 0 || index >= this.length) return null;
-
         let currentNode = this.head;
         for (let i = 0; i < index; i++) {
             currentNode = currentNode.nextNode;
@@ -59,15 +66,14 @@ class LinkedList {
     }
 
     pop() {
+        // REMOVE THE LAST ELEMENT FROM THE LIST
         if (!this.head) return null;
-
         let currentNode = this.head;
         let previousNode = null;
         while (currentNode.nextNode) {
             previousNode = currentNode;
             currentNode = currentNode.nextNode;
         }
-
         if (previousNode) {
             previousNode.nextNode = null;
             this.tail = previousNode;
@@ -80,6 +86,7 @@ class LinkedList {
     }
 
     contains(value) {
+        // RETURN TRUE IF THE PASSED IN VALUE IS IN THE LIST AND OTHERWISE RETURN FALSE
         let currentNode = this.head;
         while (currentNode) {
             if (currentNode.value === value) return true;
@@ -89,6 +96,7 @@ class LinkedList {
     }
 
     find(value) {
+        // RETURN THE INDEX OF THE NODE CONTAINING VALUE, OR NULL IF NOT FOUND
         let currentNode = this.head;
         let index = 0;
         while (currentNode) {
@@ -100,6 +108,7 @@ class LinkedList {
     }
 
     toString() {
+        // REPRESENT THE LINKED LIST OBJECT AS A STRING
         let result = '';
         let currentNode = this.head;
         while (currentNode) {
@@ -111,6 +120,7 @@ class LinkedList {
     }
 
     insertAt(value, index) {
+        // INSERT A NEW NODE WITH THE PROVIDED VALUE AT THE GIVEN INDEX
         if (index < 0 || index > this.length) return false;
         if (index === 0) {
             this.prepend(value);
@@ -120,7 +130,6 @@ class LinkedList {
             this.append(value);
             return true;
         }
-
         const newNode = new Node(value);
         let currentNode = this.head;
         let previousNode = null;
@@ -135,6 +144,7 @@ class LinkedList {
     }
 
     removeAt(index) {
+        // REMOVE THE NODE AT THE GIVEN INDEX
         if (index < 0 || index >= this.length) return null;
         if (index === 0) {
             const removedNode = this.head;
@@ -159,8 +169,6 @@ class LinkedList {
         return currentNode;
     }
 }
-
-
 
 const linkedList = new LinkedList();
 linkedList.append(1);
